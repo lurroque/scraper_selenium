e72023123b528a56dbeb27aa57d46bc3f884aeee
Luiz Roque <lrvroque@gmail.com>
2019-02-17 09:41:19 -0300

    'Refatorando'

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import Select
 import time
 
 
-driver = webdriver.Firefox(executable_path="./geckodriver")
+driver = webdriver.Firefox(executable_path="./geckodriver.exe")
 driver.maximize_window()
 driver.get("http://spys.one/free-proxy-list/BR/")
 for i in range(3):
@@ -20,13 +20,6 @@ for tr in tr_spy1xx + tr_spy1x:
     if len(colunas) > 0:
         colunas = colunas[0]
         tr_list.append(colunas)
-
-# for tr in tr_spy1x:
-#     tr_list.append(tr)
-
-# tr_spy14 = driver.find_elements_by_class_name("spy14")
-# for tr in tr_spy14:
-#     tr_list.append(tr)
 
 for tr in tr_list:
     print(tr.text)
